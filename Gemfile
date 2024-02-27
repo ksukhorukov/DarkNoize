b573fdf0b47935d124a309ad10cb17f816f608b8
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2024-02-28 04:25:15 +0800

    todo: rspecs

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@
 
 source 'https://rubygems.org'
 
-# gem 'rspec', '~> 3.12'
 gem 'colorize', '~> 1.1'
 gem 'pry', '~> 0.14.2'
 gem 'rubocop', '~> 1.60', '>= 1.60.2'
--- rspec/rspec_helper.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-%w[rspec rspec-core rspec-expectations rspec-mocks rspec-support].each do |lib|
-  gem lib, git: "https://github.com/rspec/#{lib}.git", branch: 'main'
-end
