3d8003e0f505e349c1334dcc3bd6125a38bd01ff
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2024-02-28 04:03:21 +0800

    fixed bug connected with the impossibility of urls base loading in global context

--- lib/noizer.rb
+++ lib/noizer.rb
@@ -68,12 +68,20 @@ class Noizer
     @urls ||= File.readlines(urls_file_path).map(&:strip).shuffle!
   end
 
-  def pwd
-    @current_dir ||= Dir.pwd
+  def executable_path
+    @executable_path ||= $0
+  end 
+
+  def executable_name
+    :darknoize
+  end
+
+  def project_directory 
+    @project_directory ||= executable_path.split('')[0, executable_path.length - executable_name.length].join
   end 
 
   def urls_file_path
-    @urls_file_path ||= [pwd, '/', URLS_DIR, '/', URLS_FILE].join
+    @urls_file_path ||= [project_directory, URLS_DIR, '/', URLS_FILE].join
   end 
 
   def fetch(uri, limit = 10)
